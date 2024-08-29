1b35fe88194b5d6166b4e80098905fceb9f022e4
ginavong <gina121vong@gmail.com>
2024-08-29 17:49:18 +0100

    Added ggplot functions

---
+++ R/functions/ggplot_functions.R
@@ -0,0 +1,30 @@
+# Colours
+
+led_colours = c('mediumslateblue', 'navy', 'royalblue', 'skyblue', 'forestgreen', 'darkorange', 'brown1', 'firebrick', 'black')
+
+led_colours_dark = c('mediumslateblue', 'navy', 'royalblue', 'skyblue', 'forestgreen', 'darkorange', 'brown1', 'firebrick', 'white')
+
+
+#---
+# Transparent dark theme for presentations
+
+theme_presentation = function() {
+  theme(
+    #Set black things to white
+    line = element_line(color='#ffffff'),
+    text = element_text(color="#ffffff", size=25),
+    axis.text=element_text(color="#ffffff"),
+    axis.line=element_line(color="#ffffff"),
+    axis.ticks=element_line(color="#ffffff"),
+    strip.text = element_text(colour = '#ffffff'),
+    
+    #Transparent background
+    legend.background=element_rect(fill = "transparent",colour = NA),
+    legend.key = element_rect(colour = NA, fill = NA),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA),
+    strip.background = element_rect(fill='transparent', colour='grey')
+  )
+}
