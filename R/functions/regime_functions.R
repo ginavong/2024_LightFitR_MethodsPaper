4c5d533e82c74e548093c8989b661ddd30becbb1
Gina Vong <gina121vong@gmail.com>
2024-09-02 11:43:26 +0100

    Fixed bug in regime_functions

--- R/functions/regime_functions.R
+++ R/functions/regime_functions.R
@@ -1,7 +1,7 @@
 # Make the timepoints for testing at 5mins apart
 test_times = function(nEvents){
   time_vec = seq(from=lubridate::hm('00:00'), by=lubridate::minutes(5), length.out=nEvents)
-  time_vec = as.POSIXct(time_vec, origin=origin, tz='GMT')
+  time_vec = as.POSIXct(time_vec, origin=lubridate::origin, tz='GMT')
   time_mat = LightFitR::internal.makeTimes(time_vec)
   return(time_mat)
 }
