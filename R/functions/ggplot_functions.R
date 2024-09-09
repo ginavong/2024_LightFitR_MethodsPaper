63798d343c136a1064d1c09723ab2868a77c90cc
Gina Vong <gina121vong@gmail.com>
2024-09-09 15:12:59 +0100

    Added to ggplot_functions

--- R/functions/ggplot_functions.R
+++ R/functions/ggplot_functions.R
@@ -1,12 +1,26 @@
+# Saving -----
+save_fig = function(filename, fig, dimensions = c(20, 15)){
+  
+  # png
+  fn = paste(filename, '.png', sep='')
+  ggsave(fn, plot=fig, width=dimensions[1], height=dimensions[2], units='cm', dpi=600)
+  
+  #svg
+  fn = paste(filename, '.svg', sep='')
+  ggsave(fn, plot=fig, width=dimensions[1], height=dimensions[2], units='cm', dpi=600)
+}
+
 # Colours
 
 led_colours = c('mediumslateblue', 'navy', 'royalblue', 'skyblue', 'forestgreen', 'darkorange', 'brown1', 'firebrick', 'black')
 
 led_colours_dark = c('mediumslateblue', 'navy', 'royalblue', 'skyblue', 'forestgreen', 'darkorange', 'brown1', 'firebrick', 'white')
 
+# Labels -----
+irr_w_lab = expression('irradiance (W m'^-2 * nm^-1*')')
+wl_lab = "wavelength (nm)"
 
-#---
-# Transparent dark theme for presentations
+# Transparent dark theme for presentations-----
 
 theme_presentation = function() {
   ggplot2::theme(
