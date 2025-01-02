9d9d4c5851577121a9be0dd497c2412493cdc905
Gina Vong <gina121vong@gmail.com>
2025-01-02 13:54:38 +0000

    Revert "Updated save_data function"
    This reverts commit d333803275cdca9d8962676e49c390e542d28597.

--- R/functions/processing_functions.R
+++ R/functions/processing_functions.R
@@ -1,7 +1,4 @@
-save_data = function(data, df_name, filename){
-
-  df_name = data
-
-  save(df_name, file=paste(filename, '.Rda', sep=''))
-  write.csv(data, file=paste(filename, '.csv', sep=''), row.names=F)
+save_data = function(df, filename){
+  save(df, file=paste(filename, '.Rda', sep=''))
+  write.csv(df, file=paste(filename, '.csv', sep=''), row.names=F)
 }
