a5318370593086ab48479f99e661a0e0a5747db0
Gina Vong <gina121vong@gmail.com>
2025-01-06 13:43:59 +0000

    Initial outline of grid search script

---
+++ R/regime_design/5.2_GridSearch_regime.R
@@ -0,0 +1,18 @@
+# 0. Setup ----
+
+rm(list=ls())
+
+## Libraries and functions
+
+library(LightFitR)
+source('R/functions/regime_functions.R')
+
+## Import data
+
+target = 
+  measurement = # or residual 
+  intensity = 
+  
+
+# 1. Define search range
+  
