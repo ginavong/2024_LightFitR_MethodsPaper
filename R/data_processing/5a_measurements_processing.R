1ec7286540ac6a348d78aa0c93ef9823ad7fd7a9
Gina Vong <gina121vong@gmail.com>
2025-01-16 14:35:49 +0000

    Quick and dirty processing of the random search results so I can move on with grid search

--- R/data_processing/5a_measurements_processing.R
+++ R/data_processing/5a_measurements_processing.R
@@ -11,6 +11,7 @@ wd = getwd()
 ## Functions / libraries
 library(LightFitR)
 library(lubridate)
+library(dplyr)
 
 setwd('R/functions')
 source('OceanView_functions.R')
@@ -100,7 +101,173 @@ save_data(measurements, fn)
 
 rm(fn)
 
-# 5. Calculate MSE ----
+# 5.Format refinement df ----
 
+message("5. Formatting")
 
-# 6. Find lowest MSEs for random search
+## Format refinement to keep columns we want
+
+refinement = data.frame(calibration_processing=refinement_subset$calibration_processing,
+                        stage=paste(refinement_subset$algorithm_type, refinement_subset$algorithm, sep='.'), 
+                        treat=refinement_subset$event, 
+                        LED=refinement_subset$LED, wavelength=refinement_subset$wavelength, 
+                        target=refinement_subset$target_irradiance, 
+                        intensity_used=refinement_subset$predicted_intensity)
+
+## Filter for middle points and peaks
+
+criteria = (measurements$middle_time==T) & (measurements$peak==T)
+measurements2 = measurements[criteria,]
+rm(criteria)
+
+## Add treatment column
+
+treats = unique(refinement$treat)
+
+measurements2$treat = sapply(measurements2$event, function(i){
+  
+  if((i >=1) & (i <=50)){
+    treat = treats[1]
+  } 
+  if((i>=51) & (i<=100)){
+    treat = treats[2]
+  } 
+  if((i>=101) & (i<=150)){
+    treat=treats[3]
+  }
+  
+  return(treat)
+})
+
+rm(treats)
+
+## Add target column
+
+measurements2 = left_join(measurements2, 
+                           (refinement |> select(wavelength, target, treat)), 
+                           join_by(wavelength, treat))
+
+## Add intensity_used column
+
+events = unique(measurements2$event)
+intensity_used = sapply(events, function(i){
+  regime[-c(1:4, 13), i]
+})
+
+intensity_used = as.numeric(as.vector(intensity_used))
+
+measurements2$intensity_used = intensity_used
+
+rm(events, intensity_used)
+
+## Add additional columns to measurements 2
+
+measurements2$calibration_processing = rep('none', nrow(measurements2))
+measurements2$stage = rep('random.search', nrow(measurements2))
+
+measurements2$LED = sapply(measurements2$wavelength, function(wl){
+  peaks[peaks$median_peak_wl==wl, 'LED_name']
+})
+
+## Add additional columNs to refinement
+refinement$event = rep(NA, nrow(refinement))
+refinement$measured = rep(NA, nrow(refinement))
+
+## Format measurements2 df
+
+colnames(measurements2)
+
+measurements2 = data.frame(calibration_processing = measurements2$calibration_processing,
+                           stage = measurements2$stage, 
+                           treat = measurements2$treat, event=measurements2$event,
+                           LED = measurements2$LED, wavelength = measurements2$wavelength, 
+                           intensity_used = measurements2$intensity_used,
+                           target=measurements2$target, measured=measurements2$watts)
+
+## Combine dfs
+
+refinement = rbind(refinement, measurements2)
+
+# 6. Calculate diff ----
+
+message("6. Calculate errors")
+
+refinement$diff = refinement$measured - refinement$target
+refinement$diff_squared = refinement$diff^2
+
+# 7. Calculate MSE ----
+
+## Filter algorithm raw output for now
+
+refinement = refinement[refinement$stage=='random.search',]
+
+events = unique(refinement$event)
+mse_refinement = sapply(events, function(i){
+  
+  data_subset = refinement[refinement$event==i,]
+  
+  discard_cols = which(colnames(data_subset) %in% c('LED', 'wavelength', 'target', 'intensity_used', 'measured', 'diff', 'diff_squared'))
+  
+  # Calculate mse
+  
+  mse = mean(data_subset$diff_squared)
+  
+  # Format df
+  
+  row = c(data_subset[1, -discard_cols], mse)
+  
+  setNames(row, c(colnames(data_subset)[-discard_cols], 'MSE'))
+  
+  row
+})
+
+## Formatting
+mse_refinement = as.data.frame(t(mse_refinement))
+colnames(mse_refinement)[ncol(mse_refinement)] = 'MSE'
+
+mse_refinement$calibration_processing = as.character(mse_refinement$calibration_processing)
+mse_refinement$stage = as.character(mse_refinement$stage)
+mse_refinement$treat = as.numeric(mse_refinement$treat)
+mse_refinement$event = as.numeric(mse_refinement$event)
+mse_refinement$MSE = as.numeric(mse_refinement$MSE)
+
+rm(events)
+
+# 8. Lowest MSEs for grid search ----
+
+message("8. Lowest MSE for grid search")
+
+## Find lowest events
+
+treats = unique(refinement$treat)
+lowest = t(sapply(treats, function(i){
+  data_subset = mse_refinement[mse_refinement$treat ==i,]
+  min_event = data_subset[which.min(data_subset$MSE), 'event']
+  c(i, min_event)
+}))
+lowest = as.data.frame(lowest)
+colnames(lowest) = c('treat', 'lowest_event')
+
+## Label dfs with this info
+
+mse_refinement$status = 'none'
+lowest_index = which(mse_refinement$event %in% lowest$lowest_event)
+mse_refinement[lowest_index, 'status'] = 'best'
+
+refinement$status = 'none'
+lowest_index = which(refinement$event %in% lowest$lowest_event)
+refinement[lowest_index, 'status'] = 'best'
+
+## Subset the best
+
+for_gridSearch = refinement[refinement$status=='best',]
+
+# 9. Export ----
+
+out_dir = 'data/algorithm_testing/fig5_refinement/'
+setwd(out_dir)
+
+save(refinement, mse_refinement, file='5_refinement.Rda')
+save(for_gridSearch, file='5b_forGridSearch.Rda')
+
+setwd(wd)
