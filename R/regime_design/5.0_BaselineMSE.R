5f1a0e62528cc5750baaec802d2708e7d1b6511a
Gina Vong <gina121vong@gmail.com>
2025-01-20 09:00:27 +0000

    Added script to get baseline MSE in irradiance units

---
+++ R/regime_design/5.0_BaselineMSE.R
@@ -0,0 +1,55 @@
+# 0. Setup ----
+
+rm(list=ls())
+
+## Libraries and functions
+
+library(LightFitR)
+source('R/functions/regime_functions.R')
+
+## Import data
+
+load('data/algorithm_testing/fig5_refinement/5a_SubsetForRefinement.Rda')
+
+refinement_subset
+
+# 1. Make light recipe----
+
+treat = unique(refinement_subset$event)
+
+light_recipe = sapply(treat, function(i){
+  
+  data_subset = refinement_subset[refinement_subset$event==i,]
+  
+  recipe = data_subset$predicted_intensity
+  
+  ## Set LEDs not of interest to 0
+  loc = which(data_subset$true_intensity  == 0)
+  recipe[loc] = 0
+  
+  recipe
+})
+
+# 2. Time recipe ----
+#This is sligtly different to the typical time recipe. BC there are so few points, we can have 10mins per event.
+
+nEvents = ncol(light_recipe)
+time_vec = seq(from=lubridate::hm('00:00'), by=lubridate::minutes(10), length.out=nEvents)
+time_vec = as.POSIXct(time_vec, origin=lubridate::origin, tz='GMT')
+time_mat = LightFitR::internal.makeTimes(time_vec)
+
+rm(time_vec)
+
+# 3. Regime ----
+
+regime = rbind(time_mat, light_recipe, rep(0, ncol(light_recipe)))
+rownames(regime) = c(rownames(time_mat), LightFitR::helio.dyna.leds$name)
+
+## Export
+
+fp = 'data/regimes/fig5_Refinement/'
+
+
+helio.writeSchedule(regime, paste(fp, '5.0_Baseline.csv', sep=''), format='csv')
+helio.writeSchedule(regime, paste(fp, '5.0_Baseline.txt', sep=''), format='json')
+write.csv(regime, paste(fp, '5.0_Baseline_intensities.csv'), col.names=F, row.names=T)
