f669d74540b2a72c2fb32184aa6563b7bb38aa86
Gina Vong <gina121vong@gmail.com>
2025-02-07 11:51:10 +0000

    Removed randomness from is.middle in OceanView_functions to see if this fixes the bug

--- R/functions/OceanView_functions.R
+++ R/functions/OceanView_functions.R
@@ -227,7 +227,8 @@ is.middle = function(events_vec, time_vec){
     } 
     else{ # If it's an even length of times, randomly pick either side of the median
       halfway = length(times)/2
-      rand = sample(c(halfway, halfway+1), size=1)
+      #rand = sample(c(halfway, halfway+1), size=1)
+      rand = halfway
       med = times[rand]
     }
     
