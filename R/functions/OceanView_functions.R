68bc8600245bdeb6f3a01909666ab9b3248d3e5e
Gina Vong <gina121vong@gmail.com>
2025-02-07 13:30:56 +0000

    Changed the is.middle to the other point to see if this produces a difference

--- R/functions/OceanView_functions.R
+++ R/functions/OceanView_functions.R
@@ -228,7 +228,7 @@ is.middle = function(events_vec, time_vec){
     else{ # If it's an even length of times, randomly pick either side of the median
       halfway = length(times)/2
       #rand = sample(c(halfway, halfway+1), size=1)
-      rand = halfway
+      rand = halfway +1
       med = times[rand]
     }
     
