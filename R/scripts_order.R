9dc4a779d9ec4304202d5fa08c40d68d7a4c2f6c
Gina Vong <gina121vong@gmail.com>
2025-02-07 11:56:04 +0000

    Fixed filepaths in scripts_order so that we aren't messing with working directories

--- R/scripts_order.R
+++ R/scripts_order.R
@@ -1,31 +1,26 @@
 # Setup
 rm(list=ls())
 
-wd = getwd()
-setwd('R/')
-
 # Figure 3 and Supplements
 
-source('regime_design/3.0_Calibration_regime.R')
-source('data_processing/3.1_Apollo_calibration_processing.R')
-source('analysis/3.2_fig3&sups.R')
+source('R/regime_design/3.0_Calibration_regime.R')
+source('R/data_processing/3.1_Apollo_calibration_processing.R')
+source('R/analysis/3.2_fig3&sups.R')
 
 # Figure 4 and Supplements
 
-source('regime_design/4.0_ComplexityTest_regime.R')
-source('data_processing/4.1_measurements_processing.R')
-source('data_processing/4.2_algorithmTests.R')
-source('analysis/4.3_fig4&sups.R')
+source('R/regime_design/4.0_ComplexityTest_regime.R')
+source('R/data_processing/4.1_measurements_processing.R')
+source('R/data_processing/4.2_algorithmTests.R')
+source('R/analysis/4.3_fig4&sups.R')
 
 # Figure 5 and Supplements
 
-source('regime_design/5.0_BaselineMSE.R')
-source('data_processing/5.1_measurements_processing.R')
-source('regime_design/5.2_RandomSearch_regime.R')
-source('data_processing/5.3_random_measurements_processing.R')
-source('data_processing/5.4_RefinementProcessing.R')
-source('analysis/5.5_fig5&sups.R')
+source('R/regime_design/5.0_BaselineMSE.R')
+source('R/data_processing/5.1_measurements_processing.R')
+source('R/regime_design/5.2_RandomSearch_regime.R')
+source('R/data_processing/5.3_random_measurements_processing.R')
+source('R/data_processing/5.4_RefinementProcessing.R')
+source('R/analysis/5.5_fig5&sups.R')
 
 # Reset
-
-setwd(wd)
