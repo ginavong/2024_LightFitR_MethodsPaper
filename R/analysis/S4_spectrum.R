2beb9aeff10940a877d3e0a627c1f74c5bb66969
Gina Vong <gina121vong@gmail.com>
2025-12-19 10:57:10 +0000

    Moved spectrum graphs into S4 and previous S4 figs into S5

--- R/analysis/S4_spectrum.R
+++ R/analysis/S4_spectrum.R
@@ -6,7 +6,7 @@ rm(list=ls())
 
 wd = getwd()
 
-out_path = './results/'
+out_path = './results/S4/'
 data_path = './data/heliospectra_measurements/fig4_20240905/4_annotated_20240905.Rda'
 
 functions_path = './R/functions/'
