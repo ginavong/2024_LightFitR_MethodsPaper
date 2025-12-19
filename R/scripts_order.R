8c6c712f34120de4aec3d4109dc6c8bb2545b680
Gina Vong <gina121vong@gmail.com>
2025-12-19 14:12:46 +0000

    Ran all fig6 scripts, no changes

--- R/scripts_order.R
+++ R/scripts_order.R
@@ -17,9 +17,9 @@ source('R/analysis/S4_spectrum.R') #ran this, no changes
 
 # Figure 5 and Supplements
 
-source('R/regime_design/5.0_Baseline.R')
-source('R/data_processing/5.1_measurements_processing.R')
-source('R/regime_design/5.2_RandomSearch_regime.R')
+source('R/regime_design/5.0_Baseline.R') #Ran, no changes
+source('R/data_processing/5.1_measurements_processing.R') #Ran, no changes
+source('R/regime_design/5.2_RandomSearch_regime.R') 
 source('R/data_processing/5.3_random_measurements_processing.R')
 source('R/data_processing/5.4_RefinementProcessing.R')
 source('R/analysis/5.5_fig5&sups.R')
@@ -27,8 +27,8 @@ source('R/analysis/5.5_fig5&sups.R')
 # Figure 6 and Supplements
 
 source('R/regime_design/6.0_RFR_regime.R') #Ran this, no changes
-source('R/data_processing/6.1_RFR_processing.R')
-source('R/analysis/6.2_fig6.R')
+source('R/data_processing/6.1_RFR_processing.R') #Ran this, no changes
+source('R/analysis/6.2_fig6.R') #Ran, no changes
 
 # Reset
 
